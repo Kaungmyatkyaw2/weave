ffbf97d75b1e6722cb5f8f28137ff96d4aa2edf5
Kaungmyatkyaw2 <kaungmyatkyaw2030@gmail.com>
2023-12-27 20:31:31 +0630

    feat : updated title and fav icon

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -2,7 +2,7 @@ import { BotIcon } from "lucide-react";
 
 export const Navbar = () => {
   return (
-    <nav className="lg:w-[80%] sm:w-[65%] w-full fixed top-0 right-0 h-[50px] flex items-center justify-center border-b bg-white ]">
+    <nav className="lg:w-[80%] sm:w-[65%] w-full fixed top-0 right-0 h-[50px] flex items-center justify-center border-b bg-white z-[9]">
       <button className="w-fit flex items-center space-x-[10px]">
         <BotIcon size={30} className="text-red-600" />
         <p className="text-sm text-smoke">Weave</p>
